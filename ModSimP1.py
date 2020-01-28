c0ce9b10d2aab58625f457c33c1e3c0be716621e
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-01-28 00:46:37 -0500

    Update ModSimP1.py

--- ModSimP1.py
+++ ModSimP1.py
@@ -1,23 +1,36 @@
 # ModSim P1
 
-import time 
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def generateSeed():
-    temp = int((time.time() % 1) * 10**16)
-    seed = [int(i) for i in str(temp)]
+    intSeed = int((time.time() % 1) * 10**16)
+    seed = [int(i) for i in str(intSeed)]
     return seed
 
-def laggedFibonacciGenerator(seed, ijk, num_gen=10):
-    i,j,k = ijk
+
+def laggedFibonacciGenerator(seed=None, num_gen=10):
+    output = []
+    i, j, k = 3, 7, 13
+
+    if(seed == None):
+        seed = generateSeed()
+
     for _ in range(num_gen):
         rand_num = (seed[i] + seed[j] + seed[k]) % 10
-        print(rand_num)
+        output.append((rand_num))
         seed.pop(0)
         seed = seed + [rand_num]
         # print(seed)
+    return output
+
+
+def p1(size=None, method='NR', seed=None, returnSeed=False):
+
+    return
+
 
 if __name__ == "__main__":
-    three_tap = (3,7,13)
-    seed = generateSeed()
-    print(seed)
-    laggedFibonacciGenerator(seed, three_tap)
+    temp1 = laggedFibonacciGenerator(num_gen=1000000)
