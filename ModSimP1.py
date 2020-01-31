e374822a9d54f49dec06d4c962ce7e982ee7c283
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-01-30 23:53:51 -0500

    Update ModSimP1.py

--- ModSimP1.py
+++ ModSimP1.py
@@ -13,29 +13,50 @@ def generateSeed():
 
 
 def laggedFibonacciGenerator(size, seed):
-    seed = [seed/10**16 for i in str(seed)]
-    i, j, k = (3, 7, 11)
+    ''' laggedFibonacciGenerator(size = tuple or int, seed = int)
+        Lagged Fibonacci is another random number generating algorithm.
+        Have seed list [0,1,2,3,4,...15]. At positions i,j,k = (3,7,11), 
+        the elements are summed (3+7+11 = 21) and modded to get remainder 
+        value and then append to the list. For float random numbers we
+        want the initial seed list to be of floats 0 to 1. The first
+        value of the list is also removed and all elements shift up
+        one index keeping the seed list the same size. 
+    '''
+    seed = [seed/10**16 for i in str(seed)]  # LFG needs list of seeds to work
+    i, j, k = (3, 7, 11)  # lag positions
+
+    # initially seed is just list of same seeds so randomizes and begins the cycle
     for _ in range(len(seed)*5):
         rand_num = (seed[i] + seed[j] + seed[k]) % 1
         seed.pop(0)
         seed = seed + [rand_num]
+
+    # LFG algorithm for single random float
     if(size == None):
         return (seed[i] + seed[j] + seed[k]) % 1
+
+    # LFG algorithm for nD list of random floats
     elif(type(size).__name__ == 'tuple'):
         flat_size = size[0] * size[1]
         output = np.zeros(size)
         row, col = 0, 0
+
         for _ in range(flat_size):
             rand_num = (seed[i] + seed[j] + seed[k]) % 1
             output[row][col] = rand_num
+
             if (row == size[0]-1):
                 row = 0
                 col += 1
             else:
                 row += 1
+
             seed.pop(0)
             seed = seed + [rand_num]
+
         return output
+
+    # LFG algorithm for 1D list of random floats
     else:
         output = []
         for _ in range(size):
@@ -43,10 +64,16 @@ def laggedFibonacciGenerator(size, seed):
             output.append(rand_num)
             seed.pop(0)
             seed = seed + [rand_num]
+
         return np.asarray(output)
 
 
 def LCG(size, seed, method):
+    ''' LCG(size = tuple or int, seed = int, method = NR or RANDU)
+        helper function of p1. Takes in arguments to perform either
+        LCG algorithm to create random number(s). Returns single
+        random float or random ndarray of floats.
+    '''
     if(method == 'NR'):
         a, b, c = 1664525, 1013904223, 2**32  # coefficients for NR method
     elif(method == 'RANDU'):
@@ -70,6 +97,7 @@ def LCG(size, seed, method):
         nd_output = np.zeros(size)
         count = 0
 
+        # puts random numbers back into ndarray formmat instead of 1D
         for i in range(size[0]):
             for j in range(size[1]):
                 nd_output[i][j] = output[count]
@@ -120,23 +148,32 @@ def p2(nThrows=200, method='NR'):
         numbers are from 0 to 1 and are plotted with x and y values on the
         circle and square figure. When the point is within the circle a
         circle counter will increment one. The ratio of points that 
-        landed in the circle is the estimate for pi
+        landed in the circle is the estimate for pi.
      '''
     rand = p1(size=(nThrows, 2), method=method)
     # If the distance from center is greater than the radius, point not in circle
     X, Y = rand[:, 0], rand[:, 1]
     circount = 0
+
     for i in range(nThrows):
         distance = np.sqrt((X[i]-0.5)**2 + (Y[i]-0.5)**2)
+
         if(distance <= 0.5):
             circount += 1
+
     pi_est = circount/(nThrows-circount)
+
     title = method + ' pi estimate =' + str(pi_est)
     graph2DCircle(rand, title)
+
     return pi_est
 
 
 def graph3D(rand, title):
+    ''' graph3D(rand = numpy array of shape (n, 3), title = string)
+        Function to create a 3D scatter plot of the random values.
+        Each column represents x, y, or z.
+    '''
     X = rand[:, 0]
     Y = rand[:, 1]
     Z = rand[:, 2]
@@ -145,7 +182,6 @@ def graph3D(rand, title):
     ax = fig.add_subplot(111, projection='3d')
 
     ax.scatter(X, Y, Z, c='r', marker='o')
-
     ax.set_title(title)
     ax.set_xlabel('x axis')
     ax.set_ylabel('y axis')
@@ -155,6 +191,10 @@ def graph3D(rand, title):
 
 
 def graph2DCircle(rand, title):
+    ''' graph2DCircle(rand = numpy array of shape (n, 2), title = string)
+        Function to create 2D scatter plot of random values over circle
+        and square figure.
+    '''
     ax = plt.gca()
     ax.scatter(rand[:, 0], rand[:, 1], c='lightsalmon', marker='o')
     ax.set_facecolor('bisque')
