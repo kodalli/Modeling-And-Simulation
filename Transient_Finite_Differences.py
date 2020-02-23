988747e098a402cdbbbf57aa3e717261ea4a9fd0
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-02-23 17:56:02 -0500

    transport2 finite diff

--- Transient_Finite_Differences.py
+++ Transient_Finite_Differences.py
@@ -18,6 +18,22 @@ def fourb(Fo, Bi, nodes):
     return Tf
 
 
+def other(Fo, Bi, Tinf, Ts, To):
+    T1 = oneb(Fo, Bi, Tinf, (To[0], To[1], To[4]))
+    T2 = T5 = twob(Fo, Bi, Tinf, (To[1], To[5], To[0], To[2]))
+    T9 = T3 = twob(Fo, Bi, Tinf, (To[2], To[6], To[1], To[3]))
+    T6 = fourb(Fo, Bi, (To[5], To[1], To[4], To[6], To[9]))
+    T13 = T4 = twob(Fo, Bi, Tinf, (To[3], To[7], To[2], Ts))
+    T10 = T7 = fourb(Fo, Bi, (To[6], To[2], To[5], To[7], To[10]))
+    T14 = T8 = fourb(Fo, Bi, (To[7], To[3], To[6], To[11], Ts))
+    T11 = fourb(Fo, Bi, (To[10], To[6], To[9], To[11], To[14]))
+    T15 = T12 = fourb(Fo, Bi, (To[11], To[7], To[10], To[15], Ts))
+    T16 = fourb(Fo, Bi, (To[15], To[11], To[14], Ts, Ts))
+    Tout = [T1, T2, T3, T4, T5, T6, T7, T8,
+            T9, T10, T11, T12, T13, T14, T15, T16]
+    return Tout
+
+
 def finite(Fo, Bi, Tinf, Ts, To):
     T1 = oneb(Fo, Bi, Tinf, (To[0], To[1], To[4]))
     T2 = twob(Fo, Bi, Tinf, (To[1], To[5], To[0], To[2]))
@@ -50,6 +66,7 @@ if __name__ == "__main__":
     dt = 0.01
     Bi = h*dx/k
     Fo = k*dt/(rho*cp*dx**2)
+    print(Fo)
     Tinf = 300
     Ts = 500
     To = [321.7, 324.2, 325.5, 324.2, 324.2, 327, 328.5, 327,
@@ -59,6 +76,14 @@ if __name__ == "__main__":
     for _ in range(int(t/dt)-1):
         # print(temp)
         temp = finite(Fo, Bi, Tinf, Ts, temp)
+    for i in range(4):
+        print(temp[i], temp[i+1], temp[i+2], temp[i+3])
+
+    print(To)
+    temp = other(Fo, Bi, Tinf, Ts, To)
+    for _ in range(int(t/dt)-1):
+        # print(temp)
+        temp = other(Fo, Bi, Tinf, Ts, temp)
     for i in range(4):
         print(temp[i], temp[i+1], temp[i+2], temp[i+3])
     pass
