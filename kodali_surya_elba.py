935db2ca20e7e21a0cf457171c442eebceae929e
LAPTOP-8QG0SRNR\Surya Tej Kodali <surya.kodali@gmail.com>
2020-02-25 20:59:34 -0500

    Update kodali_surya_elba.py

--- kodali_surya_elba.py
+++ kodali_surya_elba.py
@@ -1,8 +1,13 @@
+# Surya Kodali
+# CBE 5790 Modeling and Simulation
+# 2/25/2020
+# Midterm Project Elba Epidemic
+# Resources: Stackoverflow, Numpy documentation, 
+# Matplotlib documentation, and Scipy documentation
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
-import random as rand
-import pandas as pd
 
 TOTAL_POPULATION = 32000
 
@@ -14,7 +19,7 @@ def ode_model(n, k, tSpan, nMax):
     '''
     def sick_ode(t, n):
         HY, HYF, HE, HEF, SY, SE, D, V, I = n
-        dndt = np.array((
+        dndt = [
             #HY = -sick -vaccine
             -k[0]*HY*(SY+SE) -k[5]*HY*V,
             #HYF = -sick 
@@ -24,62 +29,67 @@ def ode_model(n, k, tSpan, nMax):
             #HEF = -sick 
             -k[1]*HEF*(SY+SE),
             #SY = +sick - dead - immune
-            k[0]*HY*(SY+SE) + k[0]*HYF*(SY+SE) -k[2]*SY -k[4]*(SY+SE),
+            k[0]*HY*(SY+SE) + k[0]*HYF*(SY+SE) -k[2]*SY -k[4]*SY,
             #SE = +sick - dead - immune
-            k[1]*HE*(SY+SE) + k[1]*HEF*(SY+SE) -k[3]*SE -k[4]*(SY+SE),
+            k[1]*HE*(SY+SE) + k[1]*HEF*(SY+SE) -k[3]*SE -k[4]*SE,
             #D = +dead
             k[2]*SY + k[3]*SE,
             #V = -vaccine
             -k[5]*V*(HY+HE),
             #I = +immune + vaccine
             k[4]*(SY+SE) + k[5]*V*(HY+HE)
-        ))
+        ]
         return dndt
     
     def jacob(t, n):
         HY, HYF, HE, HEF, SY, SE, D, V, I = n
-        dfdy = np.array((
+        dfdy = [
             #HY = -sick -vaccine -k[0]*HY*(SY+SE) -k[5]*HY*V
-            [-k[0]*(SY+SE) -k[5]*V, 0, 0, 0, -k[0]*HY, -k[0]*HY, 0, -k[5]*HY, 0],
+            [-k[0]*SY -k[0]*SE -k[5]*V, 0, 0, 0, -k[0]*HY, -k[0]*HY, 0, -k[5]*HY, 0],
             #HYF = -sick -k[0]*HYF*(SY+SE)
-            [0, -k[0]*(SY+SE), 0, 0, -k[0]*HYF, -k[0]*HYF, 0, 0, 0],
+            [0, -k[0]*SY -k[0]*SE, 0, 0, -k[0]*HYF, -k[0]*HYF, 0, 0, 0],
             #HE = -sick -vaccine -k[1]*HE*(SY+SE) -k[5]*HE*V
-            [0, 0, -k[1]*(SY+SE) -k[5]*V, 0, -k[1]*HE, -k[1]*HE, 0, -k[5]*HE, 0],
+            [0, 0, -k[1]*SY -k[1]*SE -k[5]*V, 0, -k[1]*HE, -k[1]*HE, 0, -k[5]*HE, 0],
             #HEF = -sick -k[1]*HEF*(SY+SE)
-            [0, 0, 0, -k[1]*(SY+SE), -k[1]*HEF, -k[1]*HEF, 0, 0, 0],
-            #SY = +sick - dead k[0]*HY*(SY+SE) + k[0]*HYF*(SY+SE) -k[2]*SY -k[4]*(SY+SE)
-            [k[0]*(SY+SE), k[0]*(SY+SE), 0, 0, k[0]*HY + k[0]*HYF -k[2] -k[4], k[0]*HY + k[0]*HYF -k[4], 0, 0, 0],
-            #SE = +sick - dead k[1]*HE*(SY+SE) + k[1]*HEF*(SY+SE) -k[3]*SE -k[4]*(SY+SE)
-            [0, 0, k[1]*(SY+SE), k[1]*(SY+SE), k[1]*HE + k[1]*HEF -k[4], k[1]*HE + k[1]*HEF -k[3] -k[4], 0, 0, 0],
+            [0, 0, 0, -k[1]*SY -k[1]*SE, -k[1]*HEF, -k[1]*HEF, 0, 0, 0],
+            #SY = +sick - dead k[0]*HY*(SY+SE) + k[0]*HYF*(SY+SE) -k[2]*SY -k[4]*SY
+            [k[0]*SY + k[0]*SE, k[0]*SY + k[0]*SE, 0, 0, k[0]*HY + k[0]*HYF -k[2] -k[4], k[0]*HY + k[0]*HYF, 0, 0, 0],
+            #SE = +sick - dead k[1]*HE*(SY+SE) + k[1]*HEF*(SY+SE) -k[3]*SE -k[4]*SE
+            [0, 0, k[1]*SY + k[1]*SE, k[1]*SY + k[1]*SE, k[1]*HE + k[1]*HEF, k[1]*HE + k[1]*HEF -k[3] -k[4], 0, 0, 0],
             #D = +dead k[2]*SY + k[3]*SE
             [0, 0, 0, 0, k[2], k[3], 0, 0, 0],
             #V = -vaccine -k[5]*V*(HY+HE)
-            [-k[5]*V, 0, -k[5]*V, 0, 0, 0, 0, -k[5]*(HY+HE), 0],
+            [-k[5]*V, 0, -k[5]*V, 0, 0, 0, 0, -k[5]*HY -k[5]*HE, 0],
             #I = +immune + vaccine k[4]*(SY+SE) + k[5]*V*(HY+HE)
-            [k[5]*V, 0, k[5]*V, 0, k[4], k[4], 0, k[5]*(HY+HE), 0]
-        ))
+            [k[5]*V, 0, k[5]*V, 0, k[4], k[4], 0, k[5]*HY + k[5]*HE, 0]
+        ]
         return dfdy
+    #print(jacob(0, n))
     
     nV0 = n[7]
     t1 = np.arange(0,30,30/(nMax))
-    result1 = integrate.solve_ivp(fun=sick_ode, t_span=(0,30), y0=n,
-                               t_eval=t1, method='Radau', jacob=jacob)
+    result1 = integrate.solve_ivp(fun=sick_ode, t_span=(np.min(t1), np.max(t1)), y0=n,
+                               t_eval=t1, method='Radau', jac=jacob)
     
     t2 = np.arange(30,tSpan,tSpan/(nMax))
-    n_res = result1.y 
-    n0 = [n_res[x][-1] for x in range(len(n_res))]
+    n0 = result1.y[:,-1]
+    # print(n0)
     n0[7] += nV0/2
-    result2 = integrate.solve_ivp(fun=sick_ode, t_span=(30,tSpan), y0=n0,
-                               t_eval=t2, method='Radau', jacob=jacob)
-    print(result1.y.shape)
-    print(result2.y.shape)
-    result = [x+y for x,y in zip(result1.y.tolist(), result2.y.tolist())]
-    time = result1.t.tolist() + result2.t.tolist()
+    # print(n0)
+    result2 = integrate.solve_ivp(fun=sick_ode, t_span=(np.min(t2), np.max(t2)), y0=n0,
+                               t_eval=t2, method='Radau', jac=jacob)
+    
+    result = [x+y for x,y in zip(result1.y.tolist(), result2.y.tolist())] # combine values for both time invtervals
+    time = result1.t.tolist() + result2.t.tolist() # combine both time intervals
     return result, time
 
 def get_r(n, k):
+    '''
+        Helper function for the Gillespie algorithm.
+        Gets the relative reaction probabilites.
+    '''
     HY, HE, HYF, HEF, SY, SE, D, V, I = n
-    r = np.array((
+    r = [
         k[0]*HY*SY,
         k[0]*HY*SE,
         k[0]*HYF*SY,
@@ -94,19 +104,20 @@ def get_r(n, k):
         k[4]*SE,
         k[5]*HY*V,
         k[5]*HE*V
-    ))
+    ]
     return r
 
 def nvsum(n, rxn_num):
     '''
+        Helper function, sums the reaction with the current number of items.
         Equations:
 
-        HY + SY -> 2SY          k0= 0.88e-5 day^-1      r0 = k[0]*HY*SY
+        HY + SY -> 2SY          k0= 1.76e-5 day^-1      r0 = k[0]*HY*SY
         HY + SE -> SY + SE                              r1 = k[0]*HY*SE
         HYF + SY -> 2SY                                 r2 = k[0]*HYF*SY
         HYF + SE -> SY + SE                             r3 = k[0]*HYF*SE
 
-        HE + SY -> SY + SE      k1= 1.76e-5 day^-1      r4 = k[1]*HE*SY
+        HE + SY -> SY + SE      k1= 0.88e-5 day^-1      r4 = k[1]*HE*SY
         HE + SE -> 2SE                                  r5 = k[1]*HE*SE
         HEF + SY -> SY + SE                             r6 = k[1]*HEF*SY
         HEF + SE -> 2SE                                 r7 = k[1]*HEF*SE
@@ -141,36 +152,51 @@ def nvsum(n, rxn_num):
     return n
 
 def Gillespie_model(n, k, tSpan, nMax):
-    '''
+    ''' 
+        Gillespie algorithm allows for a discrete stochastic model
+        of the disease. The reactions are converted to probabilites
+        and if the reaction occurs, each element is changed relative
+        to the reaction and avoiding fractional changes.
+        
         n = [HY, HE, HYF, HEF, SY, SE, D, V, I]
         k = [k0, k1, k2, k3, k4, k5]
     '''
     time = np.zeros(int(nMax))
     t = 0
-    result = []
-    for index, _ in enumerate(time):
-        r = get_r(n, k)
+    result = np.zeros((int(nMax), 9))
+    nV0 = n[7]
+    last_val = 0  
+    vacc = True         
+    for index in range(len(time)):
+        if(vacc and t>=30):
+            n[7]+=nV0/2 # add new vaccines
+            vacc = False
+            
+        r = get_r(n, k) # rxn proportional probabilities
         rtot = sum(r)
-        if(rtot == 0):
+        
+        if(rtot == 0 or t >= tSpan): # stop condition
+            last_val = index
             break
-        w = rand.random()
-        tau = np.log(w)/rtot
-        result.append(n)
+        
+        w = np.random.uniform()
+        tau = -np.log(w)/rtot # time step
+        result[index] = n
         time[index] = t
         t+=tau
-        rprob = [i/rtot for i in r]
-        csp = pd.Series(rprob).cumsum()
-        q = rand.random()
-        rxn_num = 0
+        rprob = np.array([i/rtot for i in r]) # rxn probabilities
+        csp = np.cumsum(rprob)
+        q = np.random.uniform()
+        
         for i, item in enumerate(csp):
-            if(q < item):
+            if(q < item): # if the rand num is less than cumulative prob then the rxn occurs
                 rxn_num = i
+                n = nvsum(n, rxn_num)
                 break
-        n = nvsum(n, rxn_num)
-    result = np.array(result)
-    print(result.shape)
-    return result.T, time
-
+            
+    res = result.T[:,:last_val] # stores only results till recorded time index
+    # graph(res, time[:last_val]) # graph stochastic
+    return res[6,-1] # deaths
 
 def elba(nY0=(27199, 0, 1), nE0=(4800, 0, 0), nV0=0, timeSpan=120, nMax=2e6, nRun=1):
     '''
@@ -193,12 +219,12 @@ def elba(nY0=(27199, 0, 1), nE0=(4800, 0, 0), nV0=0, timeSpan=120, nMax=2e6, nRu
 
         Equations:
 
-        HY + SY -> 2SY          k0= 0.88e-5 day^-1      r0 = k[0]*HY*SY
+        HY + SY -> 2SY          k0= 1.76e-5 day^-1      r0 = k[0]*HY*SY
         HY + SE -> SY + SE                              r1 = k[0]*HY*SE
         HYF + SY -> 2SY                                 r2 = k[0]*HYF*SY
         HYF + SE -> SY + SE                             r3 = k[0]*HYF*SE
 
-        HE + SY -> SY + SE      k1= 1.76e-5 day^-1      r4 = k[1]*HE*SY
+        HE + SY -> SY + SE      k1= 0.88e-5 day^-1      r4 = k[1]*HE*SY
         HE + SE -> 2SE                                  r5 = k[1]*HE*SE
         HEF + SY -> SY + SE                             r6 = k[1]*HEF*SY
         HEF + SE -> 2SE                                 r7 = k[1]*HEF*SE
@@ -218,11 +244,12 @@ def elba(nY0=(27199, 0, 1), nE0=(4800, 0, 0), nV0=0, timeSpan=120, nMax=2e6, nRu
         rate const k = [k0, k1, k2, k3, k4, k5]
 
     '''
+    # input error checking
     if (nRun < 1):
         raise Exception('nRun must be greater than or equal to 1')
     if(sum(nY0)+sum(nE0) != TOTAL_POPULATION):
         raise Exception(
-            'nY0 and nE0 must add up to the total population:', TOTAL_POPULATION)
+            'nY0 and nE0 must be tuples that add up to the total population:', TOTAL_POPULATION)
     if (sum(nY0) != 0.85*TOTAL_POPULATION):
         raise Exception('Young folks should be 85% of the total population')
     if (sum(nE0) != 0.15*TOTAL_POPULATION):
@@ -232,26 +259,58 @@ def elba(nY0=(27199, 0, 1), nE0=(4800, 0, 0), nV0=0, timeSpan=120, nMax=2e6, nRu
     HY0, HYF0, SY0 = nY0
     HE0, HEF0, SE0 = nE0
     D0, V0, I0 = 0, nV0, 0
-    n0 = (HY0, HYF0, HE0, HEF0, SY0, SE0, D0, V0, I0)
-    k = [0.88e-5, 1.76e-5, 0.010, 0.030, 0.100, 3.52e-6] # day^-1
+    n0 = (HY0, HYF0, HE0, HEF0, SY0, SE0, D0, V0, I0) # inital conditions
+    k = [1.76e-5, 0.88e-5, 0.010, 0.030, 0.100, 3.52e-6] # day^-1 rate consts
     
-    # ode_result, time = ode_model(n=n0, k=k, tSpan=timeSpan, nMax=nMax)
-    # print(np.array(ode_result[6]))
-    # graph(ode_result, time)
+    ode_result, time = ode_model(n=n0, k=k, tSpan=timeSpan, nMax=nMax)
     
-    gil_result, time = Gillespie_model(n=n0, k=k, tSpan=timeSpan, nMax=nMax)
-    graph(gil_result, time)
+    gil_deaths = np.zeros(nRun)
+    for i in range(nRun):
+        gil_deaths[i] = Gillespie_model(n=n0, k=k, tSpan=timeSpan, nMax=nMax)
+    
+    graph(ode_result, time, gil_deaths)
     
     return
 
-def graph(result, time):
+def graph(result, time, gil_deaths=0):
+    '''
+        Plots the results side by side. The deterministic model
+        is on the left and shows the number of items over time
+        and the stochastic model is on the right showing the
+        number of deaths per run.
+    '''
     HY, HE, HYF, HEF, SY, SE, D, V, I = result
+    # print('deaths:', D[-1])
     t = time
-    fig, ax1 = plt.subplots(1,1)
-    ax1.plot(t, I, t, V)
-    #ax2.plot(t, V)
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,6))
+    ax1.plot(t, I, t, np.add(HYF, HY), t, np.add(HEF, HEF), t, SY, t, SE, t, D)
+    ax1.set(ylim=(0,35000), xlim=(0,time[-1]), xlabel='time (days)', 
+            ylabel='number of people', 
+            title='Elba Epidemic: Continuous-variable, deterministic model')
+    labels = ('I', 'HYF+HY', 'HEF+HE', 'SY', 'SE', 'D')
+    ax1.legend(labels, loc='upper right')
+    s=('deaths:' + str(int(D[-1])))
+    ax1.text(0.2, 0.9, s, ha='center', va='center', transform=ax1.transAxes)
+    ax2.hist(gil_deaths)
+    ax2.set(xlabel='number of deaths', 
+            ylabel=('number of runs (out of ' + str(len(gil_deaths)) + ' total)'), 
+            title='Elba Epidemic: Discrete-variable, stochastic model')
     plt.show()
 
 if __name__ == '__main__':
-    elba(nY0=(27200, 0, 0), nE0=(798, 4000, 2), nV0=15000, timeSpan=120, nMax=2e6, nRun=1)
+    # v = TOTAL_POPULATION*0.30
+    # y = 27199
+    # e = 4800
+    # p = 0.15 # prob free rider
+    # ef, eh = int(v*p), e-int(v*p)
+    # yf, yh = int(v*(1-p)), y-int(v*(1-p))
+    
+    # yf, ef = int(p*y), int(p*e)
+    # yh, eh = y - yf, e - ef
+    # elba(nY0=(27200, 0, 0), nE0=(798, 4000, 2), nV0=15000, timeSpan=120, nMax=2e6, nRun=10)
+    # elba(nY0=(27199, 0, 1), nE0=(4800, 0, 0), nV0=15000, timeSpan=120, nMax=2e6, nRun=1) # o FR
+    # elba(nY0=(yh, yf, 1), nE0=(4800, 0, 0), nV0=15000, timeSpan=120, nMax=2e6, nRun=10) # yf%
+    # elba(nY0=(yh, yf, 1), nE0=(eh, ef, 0), nV0=15000, timeSpan=120, nMax=2e6, nRun=50) # ef%
+    # elba(nY0=(yh, yf, 1), nE0=(eh, ef, 0), nV0=20000, timeSpan=120, nMax=2e6, nRun=50)
+    elba(nY0=(27200, 0, 0), nE0=(4800-4002, 4000, 2), nV0=15000, timeSpan=120, nMax=2e6, nRun=50)
     pass
