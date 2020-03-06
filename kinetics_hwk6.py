c3fd1db90b57f2c0330190d8fc6f817b5ce56c71
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2020-03-05 23:42:09 -0500

    23

--- kinetics_hwk6.py
+++ kinetics_hwk6.py
@@ -2,38 +2,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
+from scipy.optimize import fsolve
 
-k1 = 10
-k2 = 0.192
-
-
-def f(t, y):
-    '''
-        Equations:
-        k1 = 10
-        k2 = 0.192
-        dCa/dt = -k1*Ca
-        dCb/dt = -k2 + k1*Ca
-
-    '''
-    Ca, Cb = y
-    df = [
-        -k1*Ca,
-        -k2 + k1*Ca
-    ]
-    return df
+# k1 = 10
+# k2 = 0.192
 
 
-time = np.arange(0, 24, 0.001)
-res = integrate.solve_ivp(fun=f, y0=(1, 0), t_span=(
-    np.min(time), np.max(time)), method='RK45', t_eval=time)
-plt.plot(res.t, res.y[1])
-plt.xlabel('time (hrs)')
-plt.ylabel('Cb')
-plt.show()
-print(res.y[1][np.where(res.t == 0.5)])
-print(res.y[0][np.where(res.t == 0.5)])
+# def f(t, y):
+#     '''
+#         Equations:
+#         k1 = 10
+#         k2 = 0.192
+#         dCa/dt = -k1*Ca
+#         dCb/dt = -k2 + k1*Ca
+
+#     '''
+#     Ca, Cb = y
+#     df = [
+#         -k1*Ca,
+#         -k2 + k1*Ca
+#     ]
+#     return df
+
+
+# time = np.arange(0, 24, 0.001)
+# res = integrate.solve_ivp(fun=f, y0=(1, 0), t_span=(
+#     np.min(time), np.max(time)), method='RK45', t_eval=time)
+# plt.plot(res.t, res.y[1])
+# plt.xlabel('time (hrs)')
+# plt.ylabel('Cb')
+# plt.show()
+# print(res.y[1][np.where(res.t == 0.5)])
+# print(res.y[0][np.where(res.t == 0.5)])
 # limit = 0
 # for index, item in enumerate(res.y[1]):
 #     if(np.abs(item - limit) < 0.001):
 #         print(res.t[index])
+
+def f(Cc, t):
+    Co = 0.5/1000
+    return - 0.9*12/np.pi * np.cos(np.pi*t/12) - 0.5*np.log(Co + 2*Cc) + 0.5*np.log(3*Co)
+
+
+t = np.arange(0, 48, 0.01)
+Cc = [fsolve(f, 0.01, i) for i in t]
+plt.plot(t, Cc)
+plt.show()
--- modsimp6.py
+++ modsimp6.py
@@ -1,56 +1,63 @@
-import numpy as np 
+import numpy as np
+
 
 class Cowboy:
-    def __init__(self, name, skill, wins, alive):
-        self.name = name
+    def __init__(self, skill=0, wins=0, alive=False):
         self.skill = skill
-        self.wins = wins 
-        self.alive = alive 
-        
+        self.wins = wins
+        self.alive = alive
+
     def kill(self):
         self.alive = False
-    
+
     def takeshot(self):
         return np.random.binomial(1, self.skill)
-    
+
+
 def p6(nsteps=1000):
-    Smithers = Cowboy('Smithers', 0.9, 0, True)
-    Johnson = Cowboy('Johnson', 0.8, 0, True)
-    Flynn = Cowboy('Flynn', 0.5, 0, True)
-    bois = [Smithers, Johnson, Flynn]
+    Smithers = Cowboy(0.9, 0, True)
+    Johnson = Cowboy(0.8, 0, True)
+    Flynn = Cowboy(0.5, 0, True)
+    trio = [Smithers, Johnson, Flynn]
     for _ in range(nsteps):
-        Flynn.alive, Johnson.alive, Smithers.alive = 1,1,1
-        
-        while(sum([person.alive for person in bois]) > 1):
-            np.random.shuffle(bois)
-            
-            for person in bois:
-                if(person.name == 'Smithers' and person.alive):
+        Flynn.alive, Johnson.alive, Smithers.alive = 1, 1, 1
+
+        while(sum([person.alive for person in trio]) > 1):
+            # "Draw Lots"
+            np.random.shuffle(trio)
+
+            for person in trio:
+                # Smithers strategy
+                if(person is Smithers and person.alive):
                     if(person.takeshot() and Johnson.alive):
                         Johnson.kill()
                     elif(person.takeshot() and Flynn.alive):
                         Flynn.kill()
-                if(person.name == 'Johnson' and person.alive):
+                # Johnson strategy
+                if(person is Johnson and person.alive):
                     if(person.takeshot() and Smithers.alive):
                         Smithers.kill()
                     elif(person.takeshot() and Flynn.alive):
                         Flynn.kill()
-                elif(person.name == 'Flynn' and person.alive):
+                # Flynn strategy
+                elif(person is Flynn and person.alive):
                     if(Smithers.alive and Johnson.alive):
+                        # Don't shoot anyone
                         continue
                     elif(Smithers.alive and Flynn.takeshot()):
                         Smithers.kill()
                     elif(Johnson.alive and Flynn.takeshot()):
                         Johnson.kill()
-                        
-        for person in bois:
+
+        for person in trio:
             if(person.alive):
-                person.wins+=1
+                person.wins += 1
+
     print(Smithers.wins, Johnson.wins, Flynn.wins)
-    # print(sum([Smithers.wins, Johnson.wins, Flynn.wins]))    
     print(Smithers.wins/nsteps, Johnson.wins/nsteps, Flynn.wins/nsteps)
     return
-        
+
+
 if __name__ == '__main__':
-    p6(100000)
+    p6(nsteps=100000)
     pass
